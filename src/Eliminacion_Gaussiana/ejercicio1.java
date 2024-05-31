0eeaa0656981fe1790faf300b489de9ab3666933
tono_ <tono_@tono>
2024-05-30 22:10:08 -0600

    TEMA 3 PROBLEMARIO

---
+++ src/Eliminacion_Gaussiana/ejercicio1.java
@@ -0,0 +1,39 @@
+package Eliminacion_Gaussiana;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        int n = 3; // Número de incógnitas
+        double[][] matrix = {
+                {3.0, -0.1, -0.2, 7.85},
+                {0.1, 7.0, -0.3, -19.3},
+                {0.3, -0.2, 10.0, 71.4}
+        };
+        double[] result = Eliminacion(matrix, n);
+        for (int i = 0; i < n; i++) {
+            System.out.println("x[" + i + "] = " + result[i]);
+        }
+    }
+
+    public static double[] Eliminacion(double[][] matrix, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                double factor = matrix[j][i] / matrix[i][i];
+                for (int k = i; k < n + 1; k++) {
+                    matrix[j][k] -= factor * matrix[i][k];
+                }
+            }
+        }
+
+        double[] result = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = matrix[i][n];
+            for (int j = i + 1; j < n; j++) {
+                result[i] -= matrix[i][j] * result[j];
+            }
+            result[i] /= matrix[i][i];
+        }
+
+        return result;
+    }
+}
